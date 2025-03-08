0e28e13be6ddf8129e2fa19d41f88183449a50b2
bauntaai-maamau <bauntaai.maamau@gmail.com>
2025-03-09 01:18:09 +1200

    javascript last modification added

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,5 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("copyright").textContent = `©${currentYear} 🌴 Bauntaai Maamau 🌴 Onotoa, Kiribati`;
+
+const lastModified = document.lastModified;
+document.getElementById("last-modified").textContent = `Last modification: ${lastModified}`;
