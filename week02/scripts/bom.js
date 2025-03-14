0b9c8c8862d44cfef052a04c7874d4abb0b0d13c
bauntaai-maamau <bauntaai.maamau@gmail.com>
2025-03-14 21:57:18 +1200

    handling dom events

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,11 +1,26 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('__________');
+const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-deleteButton.textContent = '';
-li.append(deleteButton);
-li.append(list);
+        li.textContent = input.value;
+        deleteButton.textContent = 'Delete';
+
+        li.append(deleteButton);
+
+        list.append(li);
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = '';
+        input.focus();
+
+    }
+});
