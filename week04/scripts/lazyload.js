1d06609e5c9ff1adbcf624dcb6cd20ca7b8998d0
bauntaai-maamau <bauntaai.maamau@gmail.com>
2025-04-06 15:22:38 +1200

    lazyload exercise

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,7 @@
+const currentYear = new Date().getFullYear();
+
+document.getElementById("copyright").textContent = `©${currentYear} 🌴 Bauntaai Maamau 🌴 Onotoa, Kiribati`;
+
+const lastModified = document.lastModified;
+
+document.getElementById("last-modified").textContent = `Last modification: ${lastModified}`;
