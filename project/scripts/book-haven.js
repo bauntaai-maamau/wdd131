8fab4e929b036c269ceb24790cbb9d9b8a732ac4
bauntaai-maamau <bauntaai.maamau@gmail.com>
2025-04-13 01:03:27 +1200

    books updated

--- project/scripts/book-haven.js
+++ project/scripts/book-haven.js
@@ -7,88 +7,88 @@ document.getElementById("last-modified").textContent = `Last modification: ${las
 // Book data
 const books = [
     {
-        bookName: "To Kill a Mockingbird",
-        author: "Harper Lee",
-        published: "1960, July, 11",
-        pages: 281,
-        imageUrl: "https://covers.openlibrary.org/b/id/8225261-L.jpg"
+        bookName: "Atomic Habits",
+        author: "James Clear",
+        published: "2018, October, 16",
+        pages: 320,
+        imageUrl: "images/atomic-habits.avif"
     },
     {
-        bookName: "1984",
-        author: "George Orwell",
-        published: "1949, June, 8",
-        pages: 328,
-        imageUrl: "https://covers.openlibrary.org/b/id/153541-L.jpg"
+        bookName: "CAN'T HURT ME",
+        author: "David Goggins",
+        published: "2018, November, 15",
+        pages: 364,
+        imageUrl: "images/cant-hurt-me.jpg"
     },
     {
-        bookName: "The Great Gatsby",
-        author: "F. Scott Fitzgerald",
-        published: "1925, April, 10",
-        pages: 180,
-        imageUrl: "https://covers.openlibrary.org/b/id/7222246-L.jpg"
+        bookName: "THE DIARY OF A CEO",
+        author: "Steven Bartlett",
+        published: "2023, August, 29",
+        pages: 368,
+        imageUrl: "images/diary-of-a-ceo.jpg"
     },
     {
-        bookName: "Pride and Prejudice",
-        author: "Jane Austen",
-        published: "1813, January, 28",
-        pages: 432,
-        imageUrl: "https://covers.openlibrary.org/b/id/8231994-L.jpg"
+        bookName: "The E-Myth Revisited",
+        author: "Michael E. Gerber",
+        published: "1995, March, 21",
+        pages: 288,
+        imageUrl: "images/emyth-revisited.webp"
     },
     {
-        bookName: "The Hobbit",
-        author: "J.R.R. Tolkien",
-        published: "1937, September, 21",
-        pages: 310,
-        imageUrl: "https://covers.openlibrary.org/b/id/6979861-L.jpg"
+        bookName: "Essentialism",
+        author: "Greg McKeown",
+        published: "2014, April, 15",
+        pages: 304,
+        imageUrl: "images/essentialism.jpg"
     },
     {
-        bookName: "The Catcher in the Rye",
-        author: "J.D. Salinger",
-        published: "1951, July, 16",
-        pages: 277,
-        imageUrl: "https://covers.openlibrary.org/b/id/8231856-L.jpg"
-    },
-    {
-        bookName: "Beloved",
-        author: "Toni Morrison",
-        published: "1987, September, 16",
-        pages: 324,
-        imageUrl: "https://covers.openlibrary.org/b/id/7352162-L.jpg"
-    },
-    {
-        bookName: "The Book Thief",
-        author: "Markus Zusak",
-        published: "2005, March, 14",
-        pages: 552,
-        imageUrl: "https://covers.openlibrary.org/b/id/240727-L.jpg"
-    },
-    {
-        bookName: "The Alchemist",
-        author: "Paulo Coelho",
-        published: "1988, May, 1",
-        pages: 208,
-        imageUrl: "https://covers.openlibrary.org/b/id/8235046-L.jpg"
-    },
-    {
-        bookName: "Sapiens: A Brief History of Humankind",
-        author: "Yuval Noah Harari",
-        published: "2011, February, 4",
-        pages: 443,
-        imageUrl: "https://covers.openlibrary.org/b/id/8475041-L.jpg"
-    },
-    {
-        bookName: "Educated",
-        author: "Tara Westover",
-        published: "2018, February, 20",
+        bookName: "FACTFULNESS",
+        author: "Hans Rosling",
+        published: "2018, April, 03",
         pages: 352,
-        imageUrl: "https://covers.openlibrary.org/b/id/9283621-L.jpg"
+        imageUrl: "images/factfulness.jpg"
     },
     {
-        bookName: "The Fault in Our Stars",
-        author: "John Green",
-        published: "2012, January, 10",
-        pages: 313,
-        imageUrl: "https://covers.openlibrary.org/b/id/7275231-L.jpg"
+        bookName: "How Will You Measure Your Life",
+        author: "Clayton M. Christensen",
+        published: "2012, May, 10",
+        pages: 240,
+        imageUrl: "images/how-will-you-measure-your-life.jpg"
+    },
+    {
+        bookName: "Ikigai",
+        author: "Hector Garcia",
+        published: "2016, March",
+        pages: 208,
+        imageUrl: "images/ikigai.jpg"
+    },
+    {
+        bookName: "Man's Search For Meaning",
+        author: "Viktor Frankl",
+        published: "1946",
+        pages: 115,
+        imageUrl: "images/mans-search-for-meaning.jpeg"
+    },
+    {
+        bookName: "Never Finished",
+        author: "David Goggins",
+        published: "2022, December, 04",
+        pages: 312,
+        imageUrl: "images/never-finished.webp"
+    },
+    {
+        bookName: "The 5AM Club",
+        author: "Robin Sharma",
+        published: "2018, December, 04",
+        pages: 336,
+        imageUrl: "images/5am-club.jpg"
+    },
+    {
+        bookName: "Reach Dad Poor Dad",
+        author: "Robert T. Kiyosaki",
+        published: "1997",
+        pages: 336,
+        imageUrl: "images/rich_dad.webp"
     }
 ];
 
@@ -112,7 +112,7 @@ oldLink.addEventListener("click", () => {
 
     const oldBooks = books.filter(book => {
         const yearPublished = parseInt(book.published.split(",")[0]);
-        return yearPublished < 1900;
+        return yearPublished < 1950;
     });
     createBookCard(oldBooks);
 });
